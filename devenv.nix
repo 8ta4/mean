669536c1f6d9a56e94dd419f0a7ff1869adddf5a
a <a@as-Virtual-Machine.local>
2026-08-03 15:20:55 -0700

    Enable Nix language support in devenv

--- devenv.nix
+++ devenv.nix
@@ -19,6 +19,7 @@
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
+  languages.nix.enable = true;
 
   # https://devenv.sh/processes/
   # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
