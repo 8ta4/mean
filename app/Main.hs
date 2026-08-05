74abc0551ec96aa67cf929dcbd93abdc611d3411
a <a@as-Virtual-Machine.local>
2026-08-05 15:54:19 -0700

    Decode raw JSON lines into Aeson values in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 module Main (main) where
 
+import Data.Aeson (Value, decode)
 import Data.ByteString.Lazy.Char8 qualified as Char8
 import Relude
 
 main :: IO ()
 main = do
   content <- readFileLBS "raw-wiktextract-data.jsonl"
-  let _ = Char8.lines content
+  let _ :: [Value] = mapMaybe decode $ Char8.lines content
   pure ()
