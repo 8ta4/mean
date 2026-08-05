275e68cdc7c938b22109949131d4bf12933fb4df
a <a@as-Virtual-Machine.local>
2026-08-05 15:23:10 -0700

    Update Main to load and parse the wiktionary dataset using ByteString lines

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main (main) where
 
+import Data.ByteString.Lazy.Char8 qualified as Char8
 import Relude
 
 main :: IO ()
 main = do
   content <- readFileLBS "raw-wiktextract-data.jsonl"
+  let _ = Char8.lines content
   pure ()
