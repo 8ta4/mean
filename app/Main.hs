490ccefb74e4b5ab797da3386a51271f90be14a9
a <a@as-Virtual-Machine.local>
2026-08-05 16:01:47 -0700

    Filter decoded JSON entries by English language

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,17 @@
 module Main (main) where
 
+import Control.Lens ((^?))
 import Data.Aeson (Value, decode)
+import Data.Aeson.Lens (key, _String)
 import Data.ByteString.Lazy.Char8 qualified as Char8
 import Relude
+
+isEnglish entry = case entry ^? key "lang" . _String of
+  Just "English" -> True
+  _ -> False
 
 main :: IO ()
 main = do
   content <- readFileLBS "raw-wiktextract-data.jsonl"
-  let _ :: [Value] = mapMaybe decode $ Char8.lines content
+  let _ :: [Value] = filter isEnglish $ mapMaybe decode $ Char8.lines content
   pure ()
