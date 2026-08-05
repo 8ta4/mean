864ebf55bc83cff96a7517337ddcea94c32a2e13
a <a@as-Virtual-Machine.local>
2026-08-05 16:15:48 -0700

    Introduce placeholder logic for extracting words and glosses

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main (main) where
 
 import Control.Lens ((^?))
 import Data.Aeson (Value, decode)
-import Data.Aeson.Lens (key, _String)
+import Data.Aeson.Lens (key, values, _String)
 import Data.ByteString.Lazy.Char8 qualified as Char8
 import Relude
 
@@ -11,8 +11,14 @@ isEnglish entry = case entry ^? key "lang" . _String of
   Just "English" -> True
   _ -> False
 
+processEntry entry = case entry ^? key "word" . _String of
+  Just _ -> case entry ^? key "senses" . values . key "raw_glosses" . values . _String of
+    Just _ -> []
+    _ -> []
+  _ -> []
+
 main :: IO ()
 main = do
   content <- readFileLBS "raw-wiktextract-data.jsonl"
-  let _ :: [Value] = filter isEnglish $ mapMaybe decode $ Char8.lines content
+  let _ :: [Value] = (filter isEnglish $ mapMaybe decode $ Char8.lines content) >>= processEntry
   pure ()
