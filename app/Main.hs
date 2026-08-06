0f2db06dce4450cfb3136a43a7e657641077c9c3
a <a@as-Virtual-Machine.local>
2026-08-05 17:17:24 -0700

    Move main function to the top of app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -7,24 +7,24 @@ import Data.ByteString.Lazy.Char8 qualified as Char8
 import Data.Text qualified as Text
 import Relude
 
+main :: IO ()
+main = do
+  content <- readFileLBS "raw-wiktextract-data.jsonl"
+  let _ :: [Value] = (filter isEnglish $ mapMaybe decode $ Char8.lines content) >>= processEntry
+  pure ()
+
 isEnglish :: Value -> Bool
 isEnglish entry = case entry ^? key "lang" . _String of
   Just "English" -> True
   _ -> False
-
-makePayload :: Text -> Text -> Value
-makePayload phrase gloss = object []
-
-joinGlosses :: Value -> Text
-joinGlosses = (Text.intercalate "\n") <$> (^.. key "raw_glosses" . values . _String)
 
 processEntry :: Value -> [Value]
 processEntry entry = case entry ^? key "word" . _String of
   Just phrase -> makePayload phrase <$> joinGlosses <$> (entry ^.. key "senses" . values . key "raw_glosses")
   _ -> []
 
-main :: IO ()
-main = do
-  content <- readFileLBS "raw-wiktextract-data.jsonl"
-  let _ :: [Value] = (filter isEnglish $ mapMaybe decode $ Char8.lines content) >>= processEntry
-  pure ()
+makePayload :: Text -> Text -> Value
+makePayload phrase gloss = object []
+
+joinGlosses :: Value -> Text
+joinGlosses = (Text.intercalate "\n") <$> (^.. key "raw_glosses" . values . _String)
