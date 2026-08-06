1086de1a821e446e7ce56738f2efdfe337c7b4d3
a <a@as-Virtual-Machine.local>
2026-08-05 22:09:11 -0700

    Add sample payload generation to test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,11 @@
 module Spec where
 
-import Main (loadApiKeyHeader)
+import Main (loadApiKeyHeader, makePayload)
 import Relude
 
 main :: IO ()
 main = do
   _ <- loadApiKeyHeader
-  pure ()
+  let payload = makePayload "mean" "To intend.\nTo intend, to plan (to do); to have as one's intention."
+  putTextLn "Payload:"
+  print payload
